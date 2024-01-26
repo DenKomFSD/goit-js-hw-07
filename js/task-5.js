c422d425e8655172fe63911bcc0ccb24ef90493e
iziKomSD <122015463+iziKomSD@users.noreply.github.com>
2024-01-26 12:21:44 +0200

    task 5 Done

--- js/task-5.js
+++ js/task-5.js
@@ -3,3 +3,16 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const button = document.querySelector('.change-color');
+button.addEventListener('click', changedColor);
+
+function changedColor() {
+  const body = document.body;
+  const newColor = document.querySelector('.color');
+
+  let generateColor = getRandomHexColor();
+  body.style.backgroundColor = generateColor;
+
+  newColor.textContent = generateColor;
+}
